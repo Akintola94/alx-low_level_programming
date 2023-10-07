72696efe1b65a175d6a5ff188e086f64165dcb2e
Akintola94 <akinyeleakintola94@gmail.com>
2023-10-07 03:50:39 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ * main - prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char alph(26) = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-       	for (i = 0; i < 26: i++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar(alph(i));
-		}
-	putchar("\n");
+		putchar(alp[i]);
+	}
+	putchar('\n');
 	return (0);
 }
