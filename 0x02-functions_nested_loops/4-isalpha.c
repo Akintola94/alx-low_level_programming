8f19468dca31d26527b3cdfc9bd18625bd257fca
Akintola94 <akinyeleakintola94@gmail.com>
2023-10-10 10:57:48 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_isalpha - Checks for alphabetic order
+* @c: The character to check for
+*
+* Return: On sucess 1.
+* On error, 0 is returned.
+*/
+int _isalpha(int c)
+{
+	 c = (char)c;
+
+	 if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	 	return (1);
+	else
+		return (0);
+}
