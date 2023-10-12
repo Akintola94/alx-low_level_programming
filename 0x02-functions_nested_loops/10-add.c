afa2bab5924340b4e9e6c2bcc7eaeaa888cb55ac
Akintola94 <akinyeleakintola94@gmail.com>
2023-10-12 08:55:29 -0700

    Task 10 done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,9 +6,9 @@
  * @b: int to be added to a
  * Return: sum of a and b
  */
- int add(int a, int b)
- {
- int c;
- c= a + b;
- return (c);
- }
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
