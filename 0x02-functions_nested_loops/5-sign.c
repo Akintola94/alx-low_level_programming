8df89894883eada169fe57fb7e97718f56fc6b0c
Akintola94 <akinyeleakintola94@gmail.com>
2023-10-12 07:51:13 -0700

    Task 5 done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,19 @@
 */
 int print_sign(int n)
 {
-	if (n == 0)
+	if (n > 0)
 	{
-		_putchar('0');
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{	_putchar('0');
 		return (0);
 	}
-	if (n < 0)
-	{	_putchar('_');
-		return (-1);
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (1);
 	}
-	_putchar('+');
-	return (1);
+	return (0);
 }
