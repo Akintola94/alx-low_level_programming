409a6a1a5b777abb782c07454b1b36fb4d76de52
Akintola94 <akinyeleakintola94@gmail.com>
2023-10-12 08:39:20 -0700

    Task done 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,8 +5,7 @@
 */
 void times_table(void)
 {
-int i,j,k;
-
+int i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
@@ -18,7 +17,7 @@ _putchar(k + '0');
 }
 if (k < 10 && j != 0)
 {
-_putchar('_');
+_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
