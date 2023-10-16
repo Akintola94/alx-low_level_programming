9c9c571e2f459951c2d33766f0126e29d0dbbe7e
Akintola94 <akinyeleakintola94@gmail.com>
2023-10-16 08:38:44 -0700

    Task 2 done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
- /**
-  * _strlen - calculate the length of the string
-  * @s: -string to be calculated
-  * Return: the length of the string
-  */
-
+/**
+ * _strlen - calculate the length of the string
+ * @s: -string to be calculated
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-
 int string_length = 0;
 
 while (s[string_length])
