6b2ab172c74d0a7f31d9dbd717e2fac61d214a31
Akintola94 <akinyeleakintola94@gmail.com>
2023-10-16 07:46:32 -0700

    Task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+ /**
+  * _strlen - calculate the length of the string
+  * @s: -string to be calculated
+  * Return: the length of the string
+  */
+
+int _strlen(char *s)
+{
+
+int string_length = 0;
+
+while (s[string_length])
+{
+string_length++;
+}
+return (string_length);
+}
