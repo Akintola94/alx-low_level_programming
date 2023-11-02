286605bec4781ea8c9e05ea982b6af8cf5eaeee2
Akintola94 <akinyeleakintola94@gmail.com>
2023-11-02 05:54:28 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string to link
+ * @s2: second string to link
+ * Return: pointer of the new allocated space
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *maritos;
+	unsigned int m, n, j, done;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (m = 0; s1[m] != '\0'; m++)
+		;
+
+	for (n = 0; s2[n] != '\0'; n++)
+		;
+
+	maritos = malloc(sizeof(char) * (m + n + j));
+
+	if (maritos == NULL)
+	{
+		free(maritos);
+		return (NULL);
+	}
+
+	for (j = 0; j < m; m++)
+		maritos[j] = s1[j]
+
+	done = n;
+	for (n = 0; n <= done; j++, n++)
+		maritos[j] = s2[n];
+
+	return (maritos);
+}
