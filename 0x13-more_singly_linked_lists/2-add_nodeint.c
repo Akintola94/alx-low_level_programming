de12223f2a49455ff0c9a510d9de5c546798f023
Akintola94 <akinyeleakintola94@gmail.com>
2023-11-21 08:13:53 -0800

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *add_node;
 
-	new = malloc(sizeof(listint_t));
+	add_node = malloc(sizeof(listint_t));
 	if (!add_node)
 	return (NULL);
 
