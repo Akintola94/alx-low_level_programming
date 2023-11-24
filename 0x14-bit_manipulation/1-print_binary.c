c30acec763919864e1c795baf68d0c4b95d35bab
Akintola94 <akinyeleakintola94@gmail.com>
2023-11-23 19:05:31 -0800

    Task 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,5 +2,10 @@
 #include "main.h"
 
 /**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print
  */
-
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int n;
