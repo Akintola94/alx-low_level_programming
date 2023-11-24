dff4ec670bfd2deac3507ba40b4e6697cc256f8e
Akintola94 <akinyeleakintola94@gmail.com>
2023-11-23 20:58:31 -0800

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,5 +7,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int n;
+	int b, count = 0;
+	unsigned long int div;
+
+	for (b = 63; b >= 0; b--)
+	{
+		div = n >> 1;
+
+		if (div & 1)
+		{
+			_putchar ('1')
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0')
+}
